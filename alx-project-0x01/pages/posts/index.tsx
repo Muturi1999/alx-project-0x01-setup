8f3bdf0eb710bac073700f89167b8f5e8edb0c88
mikemuturi <mikeirungumuturi@gmail.com>
2025-06-14 19:23:21 +0300

    4. More Components (Extending Post Page)

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,26 +1,54 @@
+// pages/posts/index.tsx
+
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostProps, PostData } from "@/interfaces";
+import { useState } from "react";
+
 
 interface PostsPageProps {
   posts: PostProps[];
 }
 
 const Posts: React.FC<PostsPageProps> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [postList, setPostList] = useState<PostProps[]>(posts);
+
+  const handleAddPost = (newPost: PostData) => {
+    const newPostWithId = { ...newPost, id: postList.length + 1 };
+    setPostList([newPostWithId, ...postList]);
+  };
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-4 flex-grow bg-gray-100">
-        <div className="flex justify-between items-center mb-6">
+      <main className="p-4">
+        <div className="flex justify-between mb-4">
           <h1 className="text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add Post
+          </button>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts.map((post) => (
-            <PostCard key={post.id} {...post} />
+        <div className="grid grid-cols-3 gap-4">
+          {postList.map(({ title, body, userId, id }: PostProps, key: number) => (
+            <PostCard
+              key={key}
+              title={title}
+              body={body}
+              userId={userId}
+              id={id}
+            />
           ))}
         </div>
       </main>
+
+      {isModalOpen && (
+        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+      )}
     </div>
   );
 };
