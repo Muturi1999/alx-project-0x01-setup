1b793c28c2485e45ad752ddf55ce08382c5a41a9
mikemuturi <mikeirungumuturi@gmail.com>
2025-06-14 18:48:44 +0300

    traced user index.tsx which was empty

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,10 +1,10 @@
-import React from "react"; // Add this line to be safe
-// import Button from "@/components/common/Button";
+import Button from "@/components/common/Button";
+
 const UsersPage: React.FC = () => {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold mb-4">Users</h1>
-      {/* You can use <Button /> here if needed */}
+      <Button text="Click Me" onClick={() => alert("User Clicked!")} />
     </div>
   );
 };
